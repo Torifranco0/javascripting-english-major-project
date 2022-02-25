62d2f470aa67dc0773492bdee36d3b0897033e35
Torifranco0 <95362016+Torifranco0@users.noreply.github.com>
2022-02-25 09:29:45 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,16 @@
-let userInput;
+/*let userInput;
 userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
   $("#response").html("Brilliant choice!");
 } else {
   $("#response").html("Don’t you want a burrito?");
+}
+/*
+
+let userInput;
+userInput = prompt("What is the best movie of all time?", "Give your answer here");
+if ( userInput === "Harry Potter" ) {
+$("#response").html("I knew you had amazing taste!");
+} else {
+$("#response").html("What about Harry Potter, yo?");
 }
