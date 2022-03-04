d17990d75ed4b3d9267bb023720d251d62c4c072
Torifranco0 <95362016+Torifranco0@users.noreply.github.com>
2022-03-04 09:36:40 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -27,12 +27,21 @@ blackBeans = "black";
 makeABurrito(blackBeans);
 */
 
-
+/*
 let arrayOfStrings, arrayOfNumbers, arrayMixed;
 arrayOfStrings = ["a" , "b" , "c"];
 arrayOfNumbers = [ 1, 2, 3];
 arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
 $("#response").html(arrayOfStrings[2]);
+*/
 
+let MyBurritoObject;
+MyBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  habaneroSauceSquirts: 3
+};
+$("#response").html(myBurritoObject["tortilla"]);
 
 
