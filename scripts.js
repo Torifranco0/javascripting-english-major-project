f3b6317c77eb478cddec7e90e8f9f74d824dc24b
Torifranco0 <95362016+Torifranco0@users.noreply.github.com>
2022-03-04 09:24:39 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ if ( userInput === "burrito" ) {
   $("#response").html("Don’t you want a burrito?");
 }
 */
-
+/*
 let makeABurrito;
 makeABurrito = function(beansVariable) {
 let beansResponse;
@@ -25,4 +25,13 @@ $(#response").html(beansResponse);
 let blackBeans;
 blackBeans = "black";
 makeABurrito(blackBeans);
+
+
+let arrayOfStrings, arrayOfNumbers, arrayMixed;
+arrayOfStrings = ["a" , "b" , "c"];
+arrayOfNumbers = [ 1, 2, 3];
+arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
+$("#response").html(arrayOfStrings[2]);
+
+
 
