084010ff49abbca3e851d99a478d5cb7a3970ad2
Torifranco0 <95362016+Torifranco0@users.noreply.github.com>
2022-03-25 10:12:32 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -13,7 +13,7 @@ let washingtonSquarePark, washingtonSquareParkMarker, bobstLibrary, bobstLibrary
 washingtonSquarePark = L.latLng(40.730833, -73.9975);
 washingtonSquareParkMarker = L.marker(washingtonSquarePark).bindPopup("This is Washington Square Park").addTo(firstMap);
 bobstLibrary = L.latLng(40.729444, -73.997222);
-bobstLibraryMarker = L.marker(bobstLibrary).bindpopup("This is Bobst Library").addTo(firstMap);
+bobstLibraryMarker = L.marker(bobstLibrary).bindPopup("This is Bobst Library").addTo(firstMap);
 
 let circle;
 circle = L.circle(washingtonSquarePark, {radius: 100,
