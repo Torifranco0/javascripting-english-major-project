4d59d49abd005fb0c95901b52bad4152894dde73
Torifranco0 <95362016+Torifranco0@users.noreply.github.com>
2022-03-25 10:03:27 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,4 +1,4 @@
-let firstMap;
+let firstMap, tileLayer;
 firstMap = L.map("first-map");
 tileLayer =
 L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
@@ -6,22 +6,14 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
       subdomains: "abcd",
       maxZoom: 18
     });
+tileLayer.addTo(firstMap);
+firstMap.setView([40.730833, -73.9975], 16);
 
-
-
-let washingtonSquarePark, washingtonSquareParkMarker;
-let tenThousandth, tenThousandthPolygon, thousandth, thousandthPolyline;
+let washingtonSquarePark, washingtonSquareParkMarker, bobstLibrary, bobstLibraryMarker;
 washingtonSquarePark = L.latLng(40.730833, -73.9975);
-washingtonSquareParkMarker = L.marker(washingtonSquarePark).addTo(firstMap);
-tenThousandth = [[40.7307, -73.9976], [40.7307, -73.9974], 
-                 [40.7309, -73.9974], [40.7309, -73.9976]];
-tenThousandthPolygon = L.polygon(tenThousandth, {
-                                  color: "#268bd2",
-                                  fillColor: "#fdf6e3"
-                                }
-                        ).addTo(firstMap);
-
-
+washingtonSquareParkMarker = L.marker(washingtonSquarePark).bindPopup("This is Washington Square Park").addTo(firstMap);
+bobstLibrary = L.latLng(40.729444, -73.997222);
+bobstLibraryMarker = L.marker(bobstLibrary).bindpopup("This is Bobst Library").addTo(firstMap);
 let circle;
 circle = L.circle(washingtonSquarePark, {radius: 100,
                                          color: "#859900",
@@ -29,4 +21,3 @@ circle = L.circle(washingtonSquarePark, {radius: 100,
                                          opacity: 0.9,
                                          fillOpacity: 0.25}
           ).addTo(firstMap);
-let circle;
