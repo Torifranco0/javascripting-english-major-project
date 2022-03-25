79985e2e714026bc86fa6580f834277d8d690465
Torifranco0 <95362016+Torifranco0@users.noreply.github.com>
2022-03-25 09:38:56 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -6,14 +6,7 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
       subdomains: "abcd",
       maxZoom: 18
     });
-tileLayer.addTo(firstMap);
-firstMap.setView([40.730833, -73.9975], 16);
-let washingtonSquarePark, washingtonSquareParkMarker;
-let tenThousandth, tenThousandthPolygon, thousandth, thousandthPolyline;
-washingtonSquarePark = L.latLng(40.730833, -73.9975);
-washingtonSquareParkMarker = L.marker(washingtonSquarePark).addTo(firstMap);
-tenThousandth = [[40.7307, -73.9976], [40.7307, -73.9974], 
-                 [40.7309, -73.9974], [40.7309, -73.9976]];
+
 
 thousandth = [[washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng + 0.001],
               [washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng - 0.001],
@@ -34,6 +27,13 @@ firstMap.panTo(bobstCoords);
 
 let circle;
 circle = L.circle(washingtonSquarePark, {radius: 100,
+                                         color: "#859900",
+                                         fillColor: "#cb4b16",
+                                         opacity: 0.9,
+                                         fillOpacity: 0.25}
+          ).addTo(firstMap);
+let circle;
+circle = L.circle(bobstLibrary, {radius: 100,
                                          color: "#859900",
                                          fillColor: "#cb4b16",
                                          opacity: 0.9,
